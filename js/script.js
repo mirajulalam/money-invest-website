1fe25dada5e6a62d60b11fc85d359f471cd88397
mirajul alam <alifmirajulalam@gmail.comm>
2022-02-17 20:30:22 +0600

    input added

---
+++ js/script.js
@@ -0,0 +1,46 @@
+function calculateItem() {
+    const mainInput = document.getElementById('income-field').value;
+    const foodInput = document.getElementById('food-field').value;
+    console.log(mainInput);
+    const rentInput = document.getElementById('rent-field').value;
+    console.log(mainInput);
+    const clothInput = document.getElementById('clothes-field').value;
+
+    const totalItemAmmount = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothInput);
+    const totalBalance = document.getElementById('total-balance');
+    const expencesBalance = document.getElementById('balanceId');
+    const balance = parseFloat(mainInput) - totalItemAmmount
+
+    if (isNaN(mainInput) || isNaN(foodInput) || isNaN(rentInput) || isNaN(clothInput)) {
+        alert('Please Enter the number input field');
+        return alert;
+    }
+    else if (mainInput == '' || foodInput == '' || rentInput == '' || clothInput == '') {
+        alert('Please Enter the number input field')
+    }
+    else {
+
+        totalBalance.innerText = balance
+        expencesBalance.innerText = totalItemAmmount;
+    }
+}
+
+
+function savingAmmount() {
+    const mainInput = document.getElementById('income-field').value;
+    const savingInput = document.getElementById('saving-input').value;
+    const savingText = document.getElementById('saving-ammount');
+    const remaingText = document.getElementById('remaing-balance');
+    const savingTotalAmmount = (parseFloat(mainInput) * parseFloat(savingInput)) / 100;
+    const totalBalance = document.getElementById('total-balance').innerText;
+    const remaingAmmount = totalBalance - savingTotalAmmount;
+    savingText.innerText = savingTotalAmmount;
+    remaingText.innerText = remaingAmmount;
+}
+
+document.getElementById('calculate-button').addEventListener('click', function () {
+    calculateItem();
+})
+document.getElementById('saving-btn').addEventListener('click', function () {
+    savingAmmount();
+})
